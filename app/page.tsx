70242e28e0f28d7e9896483cb89f2f4d2a02320c
Shannon Murphy <Murphy1693@gmail.com>
2023-10-10 20:53:00 -0700

    dev check

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
-
 export default function Home() {
   return (
-    <div>This is the home page on dev.</div>
+    <div>
+      <span>This is the new home page ohhhhh ya</span>
+    </div>
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     //   <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
     //     <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
@@ -109,5 +110,5 @@ export default function Home() {
     //     </a>
     //   </div>
     // </main>
-  )
+  );
 }
