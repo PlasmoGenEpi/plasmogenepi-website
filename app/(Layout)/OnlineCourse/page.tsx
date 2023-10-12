3973d3c65b7bb372f1a53b375925b63581157a53
Shannon Murphy <Murphy1693@gmail.com>
2023-10-11 22:08:38 -0700

    removed dev color

--- app/(Layout)/OnlineCourse/page.tsx
+++ app/(Layout)/OnlineCourse/page.tsx
@@ -1,28 +1,5 @@
 import Logo from "@/app/components/Logo";
 
 export default function OnlineCourse() {
-  return (
-    <div className="">
-      <div className="h-20 bg-[#FFBCBC]"></div>
-      <div className="h-20 bg-[#FFC3A0]"></div>
-      <div className="h-20 bg-[#FFBCBC]"></div>
-
-      <div className="h-20 bg-[#FFD3B6]"></div>
-      <div className="h-20 bg-[#FFBCBC]"></div>
-
-      <div className="h-20 bg-[#D9BF77]"></div>
-      <div className="h-20 bg-[#FFBCBC]"></div>
-      <div className="h-20 bg-[#F6E0B5]"></div>
-      <div className="h-20 bg-[#FFBCBC]"></div>
-      <div className="h-20 bg-[#A2D5F2]"></div>
-      <div className="h-20 bg-[#FFBCBC]"></div>
-      <div className="h-20 bg-[#679B9B]"></div>
-      <div className="h-20 bg-[#FFBCBC]"></div>
-      <div className="h-20 bg-[#F4F3F3]"></div>
-      <div className="h-20 bg-[#FFBCBC]"></div>
-      <div className="h-20 bg-[#D3E0EA]"></div>
-      <div className="h-20 bg-[#FFBCBC]"></div>
-      <div className="h-20 bg-[#6D6875]"></div>
-    </div>
-  );
+  return <div className=""></div>;
 }
