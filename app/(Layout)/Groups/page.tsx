371f4ef8b0e150456c99a29b2e43c77d91ca157e
Shannon Murphy <Murphy1693@gmail.com>
2024-01-10 17:16:20 -0800

    logo cloud

--- app/(Layout)/Groups/page.tsx
+++ app/(Layout)/Groups/page.tsx
@@ -9,55 +9,47 @@ export default function About() {
       <NavBar2 currentNav="Groups" />
       {/* <div className="py-8"></div> */}
       <div className="mx-auto mt-8 px-2 font-roboto">
-        <div className="grid [grid-template-rows:_repeat(8,_minmax(0,_1fr))]">
-          <AboutStripe
-            logo={{
-              path: "/GroupLogos/EPPIcenter_trnsprntbkg_med_UCSF.png",
-            }}
-            organization={{
-              url: "https://eppicenter.ucsf.edu/",
-              name: "EPPIcenter",
-            }}
-            stripe={true}
-          />
-          {/* <AboutStripe
-            logo={{
-              path: "/GroupLogos/NeafseyLabLogo.png",
-              height: 200,
-              width: 400,
-            }}
-            organization={{
-              url: "",
-              name: "",
-              contact: "",
-              email: "",
-            }}
-            stripe={false}
-          /> */}
-          <AboutStripe
-            logo={{
-              path: "/GroupLogos/burnet_withouttagline_orange_rgb_digitaluse.jpg",
-              height: 75,
-              width: 75,
-            }}
-            organization={{
-              url: "https://www.burnet.edu.au/research/working-groups/infectious-diseases-systems-epidemiology-group/",
-              name: "Barry Lab",
-            }}
-            stripe={false}
-          />
-          <AboutStripe
-            logo={{
-              path: "/GroupLogos/MRC-LSTHM logo.png",
-              height: 150,
-              width: 300,
-            }}
-            organization={{
-              url: "https://www.lshtm.ac.uk/research/units/mrc-gambia",
-              name: "Malaria Population Biology",
-            }}
-            stripe={true}
-          />
+        <div className="mx-auto my-16 grid max-w-6xl place-items-center gap-4 gap-y-12 md:my-24 md:grid-cols-2 md:grid-rows-3 md:gap-8 lg:grid-cols-3 lg:grid-rows-2">
+          <div className="md:-translate-y-8 md:translate-x-8 lg:-translate-y-4 lg:translate-x-0">
+            <AboutStripe
+              logo={{
+                path: "/GroupLogos/EPPIcenter_trnsprntbkg_med_UCSF.png",
+              }}
+              organization={{
+                url: "https://eppicenter.ucsf.edu/",
+                name: "EPPIcenter",
+              }}
+              stripe={true}
+            />
+          </div>
+          <div className="md:-translate-x-16 md:-translate-y-8 lg:-translate-y-8 lg:translate-x-0">
+            <AboutStripe
+              logo={{
+                path: "/GroupLogos/burnet_withouttagline_orange_rgb_digitaluse.jpg",
+                height: 75,
+                width: 75,
+              }}
+              organization={{
+                url: "https://www.burnet.edu.au/research/working-groups/infectious-diseases-systems-epidemiology-group/",
+                name: "Barry Lab",
+              }}
+              stripe={false}
+            />
+          </div>
+          <div className=" md:-translate-x-8 lg:-translate-y-4 lg:translate-x-0">
+            <AboutStripe
+              logo={{
+                path: "/GroupLogos/MRC-LSTHM logo.png",
+                height: 150,
+                width: 300,
+              }}
+              organization={{
+                url: "https://www.lshtm.ac.uk/research/units/mrc-gambia",
+                name: "Malaria Population Biology",
+              }}
+              stripe={true}
+            />
+          </div>
           <AboutStripe
             logo={{
               path: "/GroupLogos/AU_Africa_CDC_Logo.png",
@@ -70,30 +62,34 @@ export default function About() {
             }}
             stripe={false}
           />
-          <AboutStripe
-            logo={{
-              path: "/GroupLogos/IDEA_unit_logo.png",
-              height: 100,
-              width: 100,
-            }}
-            organization={{
-              url: "https://research.pasteur.fr/en/member/aimee-taylor/",
-              name: "IDEA",
-            }}
-            stripe={true}
-          />
-          <AboutStripe
-            logo={{
-              path: "/GroupLogos/IDDynamicsLogoBanner.png",
-              height: 150,
-              width: 300,
-            }}
-            organization={{
-              url: "https://www.iddynamics.jhsph.edu",
-              name: "Infectious Disease Dynamics",
-            }}
-            stripe={false}
-          />
+          <div className="md:translate-x-12 md:translate-y-8 lg:translate-x-0 lg:translate-y-12">
+            <AboutStripe
+              logo={{
+                path: "/GroupLogos/IDEA_unit_logo.png",
+                height: 100,
+                width: 100,
+              }}
+              organization={{
+                url: "https://research.pasteur.fr/en/member/aimee-taylor/",
+                name: "IDEA",
+              }}
+              stripe={true}
+            />
+          </div>
+          <div className="md:-translate-x-12 md:translate-y-4 lg:translate-x-0">
+            <AboutStripe
+              logo={{
+                path: "/GroupLogos/IDDynamicsLogoBanner.png",
+                height: 150,
+                width: 300,
+              }}
+              organization={{
+                url: "https://www.iddynamics.jhsph.edu",
+                name: "Infectious Disease Dynamics",
+              }}
+              stripe={false}
+            />
+          </div>
         </div>
       </div>
     </div>
--- app/components/AboutStripe.tsx
+++ app/components/AboutStripe.tsx
@@ -18,24 +18,13 @@ export default function AboutStripe({
   };
 }) {
   return (
-    <div
-      className={stripe ? "bg-zinc-100 flex items-center" : "flex items-center"}
-    >
-      <div className="m-auto mx-auto grid h-full max-w-2xl grow grid-cols-2 gap-2 py-4">
-        <Link href={organization.url} className="place-self-center">
-          <Image
-            height={logo?.height || 100}
-            width={logo?.width || 200}
-            alt={`${organization.name} logo`}
-            src={logo.path}
-          />
-        </Link>
-        <div className="flex grow flex-col justify-center place-self-center p-2">
-          <h3 className="py-2 text-center font-poppins text-xl font-bold md:text-left">
-            {organization.name}
-          </h3>
-        </div>
-      </div>
-    </div>
+    <Link href={organization.url} className="">
+      <Image
+        height={logo?.height || 100}
+        width={logo?.width || 200}
+        alt={`${organization.name} logo`}
+        src={logo.path}
+      />
+    </Link>
   );
 }
