108da0d204a233f15111021c6ad205a1d30300d1
Shannon Murphy <Murphy1693@gmail.com>
2024-01-11 14:55:31 -0800

    changed capitalization

--- app/components/Accordion.tsx
+++ app/components/Accordion.tsx
@@ -53,7 +53,7 @@ export default function Accordion({
           <p className="mb-4">
             By briefly reviewing the basics of malaria epidemiology, you will be
             ready to apply your existing knowledge to the more advanced concepts
-            of Genetic surveillance later in the course.
+            of genetic surveillance later in the course.
           </p>
           <ul className="mb-4 pl-8">
             <li className="list-disc">1 lecture (10 minutes)</li>
@@ -120,7 +120,7 @@ export default function Accordion({
             control, but it has some limitations. In this module, you will learn
             how genetic surveillance can complement traditional surveillance and
             epidemiological studies to fill in knowledge and data gaps. The main
-            uses cases and essential metrics of malaria Genetic surveillance are
+            uses cases and essential metrics of malaria genetic surveillance are
             also summarized.
           </p>
           <ul className="mb-4 pl-8">
@@ -346,7 +346,7 @@ export default function Accordion({
             species, TES, drug resistance, diagnostic and insecticide
             resistance. Finally, you will gain a basic insight into next-
             generation sequencing, its capabilities, and how the tools we employ
-            to analyze Genetic data operate.
+            to analyze genetic data operate.
           </p>
           <p className="mb-4">
             If you are in the{" "}
@@ -422,29 +422,21 @@ export default function Accordion({
         </div>
         <div className="py- collapse-content pl-6 pr-12">
           <p className="mb-4">
-            Now that you&apos;ve generated some Genetic data, you need to choose
+            Now that you&apos;ve generated some genetic data, you need to choose
             the right metrics. In this module, you will learn how to choose the
-            appropriate metrics derived from Genetic data depending on the
+            appropriate metrics derived from genetic data depending on the
             questions you are asking. You will also learn to match measures of
             genetic diversity to different epidemiological scenarios and how to
             assess transmission intensity and connectivity in different
             populations
           </p>
-          <p className="mb-4">Use cases include...</p>
-          {/* <p className="mb-4">
-            In this module, you will learn how to choose the appropriate metrics
-            derived from Genetic data depending on the questions you are asking.
-            You will also learn to match measures of genetic diversity to
-            different epidemiological scenarios and how to assess transmission
-            intensity and connectivity in different populations.
-          </p> */}
+
           <p className="mb-4">
             If you are in the{" "}
             <span className="font-bold">research cohort, </span>
             you will take a deeper dive into assessing within-host diversity and
             parasite connectivity using fst, Josts D, IBD and IBS.
           </p>
-          {/* <p className="mb-4">Use cases include ...</p> */}
           <div className="mb-4">
             <p>Program Cohort:</p>
             <ul className="pl-8">
@@ -511,8 +503,8 @@ export default function Accordion({
           <p className="mb-4">
             Genetic data are only as good as the surveillance system or study
             that from which they were collected. But what makes a good malaria
-            Genetic surveillance study? In this module, you will start to learn
-            how to design effective Genetic surveillance studies. Topics study
+            genetic surveillance study? In this module, you will start to learn
+            how to design effective genetic surveillance studies. Topics study
             design, sampling, stratification, power and sample size. You will
             also learn how to critically assess the pros and cons of different
             approaches fto study design for different use cases.
@@ -676,10 +668,10 @@ export default function Accordion({
         </div>
         <div className="py- collapse-content pl-6 pr-12">
           <p className="mb-4">
-            The goal of malaria Genetic surveillance is to design and deliver
+            The goal of malaria genetic surveillance is to design and deliver
             more effective control programs, which will prevent illness and
             death. In this final module, you will focus on turning insights
-            gleaned from malaria Genetic data into actionable public health
+            gleaned from malaria genetic data into actionable public health
             recommendations. You will learn how to correctly interpret
             combinations of results, choose the best way to present your
             findings to different stakeholder, and avoid common pitfalls that
