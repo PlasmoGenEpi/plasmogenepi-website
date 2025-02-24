9157fdec49b9a862ac4dfb11664ef27ad57c0291
Shannon <murphy1693@gmail.com>
2025-02-24 06:51:38 -0800

    removed dev mode

--- app/Interactives/Sleuthing/2.6/page.tsx
+++ app/Interactives/Sleuthing/2.6/page.tsx
@@ -1,11 +1,5 @@
-"use client";
 import InteractiveViewer from "@/app/components/Interactives/Shared/InteractiveViewer/InteractiveViewer";
-import { useSearchParams } from "next/navigation";
 
 export default async function Page() {
-  const searchParams = useSearchParams();
-
-  const dev = searchParams.get("dev");
-
-  return <InteractiveViewer module={"2.6"} dev={dev ? true : false} />;
+  return <InteractiveViewer module={"2.6"} />;
 }
--- app/Interactives/Sleuthing/5.6/page.tsx
+++ app/Interactives/Sleuthing/5.6/page.tsx
@@ -1,11 +1,5 @@
-"use client";
 import InteractiveViewer2 from "@/app/components/Interactives/Shared/InteractiveViewer/InteractiveViewer2/InteractiveViewer2";
-import { useSearchParams } from "next/navigation";
 
 export default function Page() {
-  const searchParams = useSearchParams();
-
-  const dev = searchParams.get("dev");
-
-  return <InteractiveViewer2 dev={dev ? true : false} />;
+  return <InteractiveViewer2 />;
 }
