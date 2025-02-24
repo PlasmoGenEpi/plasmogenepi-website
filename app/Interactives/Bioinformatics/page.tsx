3c8b00952ca1f9a8f7291ea85f9fedfca924d15f
Shannon <murphy1693@gmail.com>
2025-02-24 07:00:42 -0800

    removed unused import

--- app/Interactives/Bioinformatics/page.tsx
+++ app/Interactives/Bioinformatics/page.tsx
@@ -1,15 +1,5 @@
 import InteractiveViewer3 from "@/app/components/Interactives/Shared/InteractiveViewer/InteractiveViewer3/InteractiveViewer3";
-import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 
 export default function Bioinformatics() {
-  // return (
-  //   <div>
-  //     <div className="h-40 bg-black"></div>
-  //     <div className="relative">
-  //       <InteractiveViewer3 />;
-  //     </div>
-  //   </div>
-  // );
-
   return <InteractiveViewer3 />;
 }
