aa4a2600b69b348227d61d44203fa5aac83f0bce
Shannon <murphy1693@gmail.com>
2025-02-24 06:54:25 -0800

    changed bio

--- app/Interactives/Bioinformatics/page.tsx
+++ app/Interactives/Bioinformatics/page.tsx
@@ -1,6 +1,5 @@
 import InteractiveViewer3 from "@/app/components/Interactives/Shared/InteractiveViewer/InteractiveViewer3/InteractiveViewer3";
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
-import { useSearchParams } from "next/navigation";
 
 export default function Bioinformatics() {
   // return (
@@ -11,12 +10,6 @@ export default function Bioinformatics() {
   //     </div>
   //   </div>
   // );
-
-  const searchParams = useSearchParams();
-
-  const dev = searchParams.get("dev");
-
-  console.log("DEV", dev);
 
   return <InteractiveViewer3 />;
 }
