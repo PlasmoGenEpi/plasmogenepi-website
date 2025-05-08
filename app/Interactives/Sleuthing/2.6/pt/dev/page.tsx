c578f7f440c94b74574f9bf02ee163b68591ece2
Shannon <murphy1693@gmail.com>
2025-05-08 08:34:29 -0700

    fixed pt dev

--- app/Interactives/Sleuthing/2.6/pt/dev/page.tsx
+++ app/Interactives/Sleuthing/2.6/pt/dev/page.tsx
@@ -1,5 +1,5 @@
 import InteractiveViewer from "@/app/components/Interactives/Shared/InteractiveViewer/InteractiveViewer";
 
 export default async function Page() {
-  return <InteractiveViewer module={"2.6"} lang={"FR"} dev />;
+  return <InteractiveViewer module={"2.6"} lang={"PT"} dev />;
 }
